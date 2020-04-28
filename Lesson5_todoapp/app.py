af34a54d73d16fd390bd74ab01daa3e3eb3cf8c7
Burakitbumrung <Deemarc.Burakitbumrung@thomsonreuters.com>
2020-04-29 00:44:33 +0700

    using sync

--- Lesson5_todoapp/app.py
+++ Lesson5_todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,24 +12,33 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
 
-"""
-# first try without database
-"""
-
-@app.route('/')
-def index():
-    data_todo = [
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'}
-    ]
-    return render_template('index.html', data = data_todo)
-
 # """
-# # second try with database
+# # first try without database
 # """
 
 # @app.route('/')
 # def index():
-#     data_todo = Todo.query.all()
+#     data_todo = [
+#         {'description': 'Todo 1'},
+#         {'description': 'Todo 2'},
+#         {'description': 'Todo 3'}
+#     ]
 #     return render_template('index.html', data = data_todo)
+
+"""
+# second try with database
+"""
+
+@app.route('/v1')
+def index1():
+    data_todo = Todo.query.all()
+    return render_template('index.html', data = data_todo)
+
+@app.route('/v1/todos/create', method=['POST'])
+def create_todo():
+  description = request.form.get('description', '')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return redirect(url_for('index1'))
+
