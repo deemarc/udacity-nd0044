213dde467baa9f37ebcc38d8fb9e6b572b4f044e
Burakitbumrung <Deemarc.Burakitbumrung@thomsonreuters.com>
2020-05-01 16:34:37 +0700

    lesson5 is updated so do lesson6

--- Lesson6_todoapp_withFlaskMigrate/app.py
+++ Lesson6_todoapp_withFlaskMigrate/app.py
@@ -1,47 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+import sys    
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
 
-# """
-# # first try without database
-# """
+db.create_all()
 
-# @app.route('/')
-# def index():
-#     data_todo = [
-#         {'description': 'Todo 1'},
-#         {'description': 'Todo 2'},
-#         {'description': 'Todo 3'}
-#     ]
-#     return render_template('index.html', data = data_todo)
-
-"""
-# second try with database
-"""
-
-@app.route('/v1')
-def index1():
+@app.route('/')
+def index():
     data_todo = Todo.query.all()
-    return render_template('index.html', data = data_todo)
+    return render_template('index_sync.html', data = data_todo)
 
-@app.route('/v1/todos/create', method=['POST'])
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
-  # description = request.form.get('description', '')
-  description = request.get_json()['description']
+  description = request.form.get('description', '')
+  # description = request.get_json()['description']
   todo = Todo(description=description)
   db.session.add(todo)
   db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  return redirect(url_for('index'))
+
+@app.route('/ajax')
+def index2():
+    data_todo = Todo.query.all()
+    return render_template('index.html', data = data_todo)
+
+@app.route('/ajax/todos/create', methods=['POST'])
+def create_todo2():
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
 
