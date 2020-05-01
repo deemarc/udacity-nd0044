7a21fb870569890b78ceacbb33fb184114a8e5ae
deemarc <marteleus@gmail.com>
2020-05-01 16:15:34 +0700

    lesson5 are all done and fixed bug

--- Lesson5_todoapp/app.py
+++ Lesson5_todoapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys    
 
 app = Flask(__name__)
 
@@ -31,19 +32,46 @@ db.create_all()
 # second try with database
 """
 
-@app.route('/v1')
-def index1():
+@app.route('/')
+def index():
     data_todo = Todo.query.all()
-    return render_template('index.html', data = data_todo)
+    return render_template('index_sync.html', data = data_todo)
 
-@app.route('/v1/todos/create', method=['POST'])
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
-  # description = request.form.get('description', '')
-  description = request.get_json()['description']
+  description = request.form.get('description', '')
+  # description = request.get_json()['description']
   todo = Todo(description=description)
   db.session.add(todo)
   db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  return redirect(url_for('index'))
+
+@app.route('/ajax')
+def index2():
+    data_todo = Todo.query.all()
+    return render_template('index.html', data = data_todo)
+
+@app.route('/ajax/todos/create', methods=['POST'])
+def create_todo2():
+  error = False
+  body = {}
+  print('hello')
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description2=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    print('erroor')
+    abort (400)
+  else:
+    return jsonify(body)
+
 
